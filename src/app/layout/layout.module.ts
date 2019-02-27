ebba7d76a9194eedc34b844ecb74691277a92ccb
simon-an <nomisimon@posteo.de>
2019-02-27 15:27:40 +0100

    ex 6.2 finish

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,16 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderWithSidenavComponent } from './header-with-sidenav/header-with-sidenav.component';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
+const imports = [MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule];
+
 @NgModule({
   declarations: [HeaderWithSidenavComponent],
-  imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-  ],
-  exports: [HeaderWithSidenavComponent],
+  imports: [CommonModule, ...imports],
+  exports: [HeaderWithSidenavComponent, ...imports],
 })
 export class LayoutModule {}
--- src/app/views/user/user.module.ts
+++ src/app/views/user/user.module.ts
@@ -5,11 +5,23 @@ import { UserRoutingModule } from './user-routing.module';
 import { UserLandingPageComponent } from './user-landing-page/user-landing-page.component';
 import { LayoutModule } from '~layout/layout.module';
 import { SafeModule } from '~safe/safe.module';
-import { MatListModule, MatIconModule, MatDividerModule } from '@angular/material';
+import { MatListModule, MatIconModule, MatDividerModule, MatToolbarModule } from '@angular/material';
 import { SafePageComponent } from './containers/safe-page/safe-page.component';
+import { AddSafeItemDialogComponent } from './containers/add-safe-item-dialog/add-safe-item-dialog.component';
 
 @NgModule({
-  declarations: [UserLandingPageComponent, SafePageComponent],
-  imports: [CommonModule, UserRoutingModule, LayoutModule, SafeModule, MatListModule, MatIconModule, MatDividerModule],
+  declarations: [UserLandingPageComponent, SafePageComponent, AddSafeItemDialogComponent],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    LayoutModule,
+    SafeModule,
+    MatListModule,
+    MatIconModule,
+    MatDividerModule,
+    MatToolbarModule,
+  ],
+  exports: [AddSafeItemDialogComponent],
+  entryComponents: [AddSafeItemDialogComponent],
 })
 export class UserModule {}
