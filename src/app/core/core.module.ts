4a53691a1082e8bd50e458621c1f8c43e1f8dfd9
simon-an <nomisimon@posteo.de>
2019-02-28 16:27:47 +0100

    ex 11.1

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { environment } from 'environments/environment';
+import { HttpClientInMemoryWebApiModule, InMemoryDbService } from 'angular-in-memory-web-api';
+import { SafeInMemDataService } from './services/in-memory-safe.service';
+import { TimingInterceptor } from './interceptors/timing.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    environment.production
+      ? []
+      : HttpClientInMemoryWebApiModule.forRoot(SafeInMemDataService, {
+          delay: 500,
+          dataEncapsulation: false,
+          passThruUnknownUrl: true,
+        }),
+  ],
+  providers: [
+    { provide: SafeInMemDataService, useExisting: InMemoryDbService },
+    { provide: HTTP_INTERCEPTORS, useClass: TimingInterceptor, multi: true },
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
